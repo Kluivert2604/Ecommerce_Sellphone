917b5487a1219420f0912ff28556658911e63bcb
PC <PC@KIEUHUNG>
2023-09-17 08:57:24 +0700

    Init Commit

---
+++ app/src/main/java/com/kieudatquochung/ecommercesellphone/Activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.kieudatquochung.ecommercesellphone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kieudatquochung.ecommercesellphone.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/kieudatquochung/ecommercesellphone/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kieudatquochung.ecommercesellphone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kieudatquochung.ecommercesellphone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
